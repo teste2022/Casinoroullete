040db8743b9c3021911998c8636a87eb203d0f09
MARCOS <100168470+teste2022@users.noreply.github.com>
2024-05-30 17:05:04 -0300

    Add files via upload

---
+++ config/steam-auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+     * Redirect URL after login
+     */
+    'redirect_url' => '/auth/login',
+    /* 
+     * API Key (http://steamcommunity.com/dev/apikey) 
+     */
+    'api_key' => '269882B10E995FF52FDA0B7EA7A45697',
+    /*
+     * Is using https ?
+     */
+    'https' => false
+
+];
